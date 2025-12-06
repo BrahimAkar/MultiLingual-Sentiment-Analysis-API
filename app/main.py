6cecf4cbca55e259ac75d0ba2f7ab9aeef4746fa
Brahim Akarouch <brahim.akarouch@gmail.com>
2025-12-06 19:38:47 +0100

    feat(main.py): add API key authentication to secure the analyze endpoint chore(docker-compose): update docker-compose files to configure network settings and remove port mapping from the main file

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Security
+from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
 from .model import predict_sentiment
 
+import os
+
 app = FastAPI()
+
+
+API_KEY =  os.getenv("SECRET_KEY")  
+API_KEY_NAME = "X-Internal-Token"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+def verify_api_key(api_key: str = Security(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Not authorized :(")
+    return api_key
+
+
 
 class TextInput(BaseModel):
     text: str
@@ -12,7 +28,7 @@ def read_root():
     return {"message": "Sentiment API is running"}
 
 
-@app.post("/analyze")
+@app.post("/analyze", dependencies=[Security(verify_api_key)])
 def analyze(data: TextInput):
     try:
         if not data.text.strip() or len(data.text.strip()) < 5:
